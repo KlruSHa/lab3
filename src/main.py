ac9114c9e157b834f880dd22ba9f5bd5071b6109
Чичеркоза Кирилл <krchicherkoza@mai.education>
2025-12-01 21:31:11 +0300

    Доделал стек

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from errors import *
+
+
 class Stack:  # Обычная реализация стека
     """
     Класс, реализующий стек, с основными методами.
@@ -7,21 +9,47 @@ class Stack:  # Обычная реализация стека
     1. push - добавляет элемент в стек
     2. pop - удаляет элемент из стека
     3. is_empty - проверяет пустой ли стек
+    4. peek - показывает верхний эелмент стека
+    5. __len__ - возвращает длину стека
+    6. min - возвращает текущий минимум стека за O(1)
     """
 
     def __init__(self, *args):
         self.items = []
+        self.min_item = []
 
     def push(self, item):
         self.items.append(item)
+        if not self.min_item:
+            self.min_item.append(item)
+        else:
+            if self.min_item[-1] >= item:
+                self.min_item.append(item)
 
     def pop(self):
         if not self.items:
-            raise IndexError("Недостаточно операндов")
-        return self.items.pop()
+            raise IndexError("Удаление из пустого стека")
+        if self.items[-1] == self.min_item[-1]:
+            self.min_item.pop()
+            return self.items.pop()
+        else:
+            return self.items.pop()
 
     def is_empty(self):
         return (self.items == [])
+
+    def peek(self):
+        if not self.items:
+            raise IndexError("Просмотр пустого стека")
+        return self.items[-1]
+
+    def __len__(self):
+        return len(self.items)
+
+    def min(self) -> int:
+        if not self.min_item:
+            raise IndexError("Стек пуст")
+        return self.min_item[-1]
 
 
 def factorial(n: int) -> int:
